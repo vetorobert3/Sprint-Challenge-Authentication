63175c6a576557fdce255ff646ab97601ef0d572
Veto Ramirez <vetorobert3@gmail.com>
2020-06-17 23:06:17 -0700

    got last test. Need to run it, though.

--- auth/auth-router.spec.js
+++ auth/auth-router.spec.js
@@ -5,7 +5,7 @@ const request = require('supertest');
 
 describe('testing API calls', () => {
 
-  describe("Add a user", () => {
+  describe("Register tests", () => {
     describe("register()", () => {
       it("should register a new user", async () => {
         await User.add({ username: "veto3", password: "password" });
@@ -15,9 +15,20 @@ describe('testing API calls', () => {
         expect(user).toHaveLength(10);
       })
     })
+
+        describe('has user property', () => {
+      test('registers successfully with res 201', () => {
+        return request(server)
+          .post('/api/auth/register')
+          .send({ "username": "lucie1", "password": "dance" })
+          .then(res => {
+            expect(res.status).toBe(201)
+          })
+      })
+    })
   })
 
-  describe('has users', () => {
+  describe('users successfully registered', () => {
     describe('has user property', () => {
       test('registers successfully with res 201', () => {
         return request(server)
@@ -26,6 +37,19 @@ describe('testing API calls', () => {
           .then(res => {
             expect(res.status).toBe(201)
           })
+      })
+    })
+  })
+
+  describe('login test', () => {
+    describe('successful login', () => {
+      it('should login successfully', () => {
+        return request(server)
+        .post('/api/auth/login')
+        .send({ "username": "APerson", "password": "5564" })
+        .then(res => {
+          expect(res.status).toBe(200)
+        });
       })
     })
   })
